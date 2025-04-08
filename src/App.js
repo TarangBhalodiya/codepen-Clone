37b2fd0c4da681ff74bee790f68f5047fc8299e5
TarangBhalodiya <tarangbhalodiya1912@gmail.com>
2025-04-08 19:21:45 +0530

    UI improvements, Suggessions functionality done

--- src/App.js
+++ src/App.js
@@ -1,59 +1,58 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Editor from './component/Editor';
-import useLocalStorage from './hooks/useLocalStorage';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Editor from "./component/Editor";
+import useLocalStorage from "./hooks/useLocalStorage";
 function App() {
+  const [html, sethtml] = useLocalStorage("html", "");
+  const [css, setcss] = useLocalStorage("css", "");
+  const [js, setjs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
 
-  const [html, sethtml] = useLocalStorage('html','')
-  const [css, setcss] = useLocalStorage('css','')
-  const [js, setjs] = useLocalStorage('js','')
-  const [srcDoc, setSrcDoc] = useState('')
-
-  useEffect(()=>{
-    var timeout = setTimeout(()=>{
+  useEffect(() => {
+    var timeout = setTimeout(() => {
       setSrcDoc(`
   <html>
   <body>${html}</body>
   <style>${css}</style>
   <script>${js}</script>
   </html>
-  `)
-    },250)
+  `);
+    }, 250);
 
-    return ()=> clearTimeout(timeout)
-  },[html,css,js])
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
     <>
-      <div className='pane top'>
-        <Editor 
-        language= "xml"
-        displayName= "HTML"
-        value = {html} 
-        onChange = {sethtml}
+      <div className="pane top">
+        <Editor
+          language="htmlmixed"
+          displayName="HTML"
+          value={html}
+          onChange={sethtml}
         />
         <Editor
-        language= "css"
-        displayName= "CSS"
-        value = {css} 
-        onChange = {setcss}
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setcss}
         />
         <Editor
-        language= "javascript"
-        displayName= "Javascript"
-        value = {js} 
-        onChange = {setjs}
+          language="javascript"
+          displayName="Javascript"
+          value={js}
+          onChange={setjs}
         />
       </div>
-      <div className='pane bottom'>
-      <iframe
-      srcDoc = {srcDoc}
-      title = "output"
-      sandbox = "allow-scripts"
-      frameBorder= "0"
-      width = "100%"
-      height = "100%"
-      />
+      <div className="pane bottom">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
       </div>
     </>
   );
--- src/component/Editor.js
+++ src/component/Editor.js
@@ -1,42 +1,52 @@
-import React from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/css/css'
-import 'codemirror/mode/javascript/javascript'
-import { Controlled as  ControlledEditor } from 'react-codemirror2'
-import { useState } from 'react'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faCompressAlt , faExpandAlt} from "@fortawesome/free-solid-svg-icons"
+import React from "react";
+import CodeMirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/html-hint";
+import "codemirror/addon/hint/css-hint";
+import "codemirror/addon/hint/javascript-hint";
+import "codemirror/mode/htmlmixed/htmlmixed";
 
-export default function Editor(props) {
+export default function Editor({ language, displayName, value, onChange }) {
+  const [open, setOpen] = useState(true);
 
-    const {language, 
-        displayName, 
-        value, 
-        onChange
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
 
-    }= props
-
-    const [open , setOpen] = useState(true)
-
-        function handleChange(editor, data, value){
-            onChange(value)
-        }
+  function handleEditorDidMount(editor) {
+    editor.on("inputRead", (cm, event) => {
+      if (!cm.state.completionActive && event.text[0].match(/[\w<]/)) {
+        CodeMirror.commands.autocomplete(cm, null, { completeSingle: false });
+      }
+    });
+  }
 
   return (
-      <div className={`editor-container ${open? "":"collapsed"}`}>
-        <div className='editor-title'>
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
         {displayName}
         <button
-        type='button'
-        className='expandbtn'
-        onClick={()=>{setOpen(preOpen => !preOpen)}}>
-            <FontAwesomeIcon icon={open? faCompressAlt:faExpandAlt}/>
+          type="button"
+          className="expandbtn"
+          onClick={() => {
+            setOpen((preOpen) => !preOpen);
+          }}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
         </button>
-        </div>
-            {/* <ControlledEditor
+      </div>
+      {/* <ControlledEditor
             onBeforeChange = {handleChange}
             value = {value}
             className = "code-mirror-wrapper"
@@ -49,18 +59,22 @@ export default function Editor(props) {
             }}
             /> */}
 
-<ControlledEditor
+      <ControlledEditor
         onBeforeChange={handleChange}
         value={value}
         className="code-mirror-wrapper"
         options={{
-            lineWrapping: true,
-            lint: true,
-            mode: language,
-            theme: "material",
-            lineNumbers: true,
+          lineWrapping: true,
+          lint: true,
+          mode: { name: language, globalVars: true },
+          theme: "material",
+          lineNumbers: true,
+          extraKeys: {
+            "Ctrl-Space": "autocomplete",
+          },
         }}
+        editorDidMount={handleEditorDidMount}
       />
-      </div>
-  )
+    </div>
+  );
 }
